0f35b0eeb8a63aa8e1c03f606892ce77d7b8b03c
chaips <ar0439708@gmail.com>
2019-11-26 12:28:52 -0600

    Se agrego el metodo onClickListener en los eventos como maniobras y combustible

--- RepartoSAHUAYO/app/src/main/java/com/example/repartosahuayo/ui/Gasto/Gastos.java
+++ RepartoSAHUAYO/app/src/main/java/com/example/repartosahuayo/ui/Gasto/Gastos.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.repartosahuayo.R;
 
@@ -14,6 +16,7 @@ import com.example.repartosahuayo.R;
  * A simple {@link Fragment} subclass.
  */
 public class Gastos extends Fragment {
+    private ImageView maniobra;
 
 
     public Gastos() {
@@ -25,7 +28,22 @@ public class Gastos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.gastos, container, false);
+        View rootView = inflater.inflate(R.layout.gastos, container, false);
+        Maniobras(rootView);
+        return rootView;
+    }
+    public void Maniobras(View view){
+        maniobra = view.findViewById(R.id.imageView3);
+        maniobra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment man = new Maniobras();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, man);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
     }
 
 }
