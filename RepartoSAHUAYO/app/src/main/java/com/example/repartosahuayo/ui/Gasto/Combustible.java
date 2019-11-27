cbee60c08e8c09ca93facc7f48f9d13e3514ea89
tafolla <tafolla.1309@gmail.com>
2019-11-27 12:25:34 -0600

    Falta programar el BackPressed en el Mensaje de ALerta!

--- RepartoSAHUAYO/app/src/main/java/com/example/repartosahuayo/ui/Gasto/Combustible.java
+++ RepartoSAHUAYO/app/src/main/java/com/example/repartosahuayo/ui/Gasto/Combustible.java
@@ -2,6 +2,7 @@ package com.example.repartosahuayo.ui.Gasto;
 
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,6 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Chronometer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.example.repartosahuayo.R;
@@ -31,24 +35,14 @@ public class Combustible extends Fragment {
         View rootView=inflater.inflate(R.layout.fragment_combustible, container, false);
         Chronometer simpleChronometer = (Chronometer)rootView.findViewById(R.id.simpleChronometer); // initiate a chronometer
         simpleChronometer.start();
-        //Mensaje(rootView);
+        Mensaje(rootView);
         return rootView;
     }
-    /*public Dialog onCreateDialog(View savedInstanceState){
-        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialogo).setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //DAR CLIK EN EL BOTON
-            }
-        });
-        builder.setMessage();
-        return builder.create();
-    }*/
+//Cuando le de Retroceso mandar Alerta
     public void Mensaje(View v){
         AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
-        builder.setMessage("DIALOGO")
-                .setTitle("DIALOG")
+        builder.setMessage("NO PUEDE REGRESAR SIN ANTES TERMINAR EL PROCESO")
+                .setTitle("ALERTA")
                 .setCancelable(false)
                 .setNeutralButton("Aceptar",
                         new DialogInterface.OnClickListener() {
